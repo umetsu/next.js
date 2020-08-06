cd11ab5882a3adc7d0ba66a52bf5cbd2a4db5a44
umetsu <prunusmume0727@gmail.com>
2020-08-06 12:58:29 +0900

    スタイルの変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import React from 'react'
 import 'highlight.js/styles/vs2015.css'
-import '../global.css'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   React.useEffect(() => {
